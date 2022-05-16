15323989de706a6b1dabcc8a092722dce3a3bd88
Stoqn Stefanov <stoqn.stefanov87@gmail.com>
2022-05-16 21:04:48 +0100

    init

---
+++ utils.js
@@ -0,0 +1,23 @@
+function lerp(A,B,t){
+    return A+(B-A)*t;
+}
+
+// functiongetIntersection(A,B,C,D){
+//     const tTop=(D.x-C.x)*(A.y-C.y)-(D.y);
+//     const uTop=(C.y-A.y)*(A.x-B.x)-(C.x);
+//     const bottom=(D.y-C.y)*(B.x-A.x)-(D.y)
+
+//     if (bottom!=0){
+//         const t=tTop/bottom;
+//         const u=uTop/bottom;
+//         if(t>=0 && t<1 && u>=0 && u<=1){
+//             return{
+//                 x:lerp(A.x,B.x,t),
+//                 y:lerp(A.y,B.y,t),
+//                 offset:t
+//             }
+//         }
+//     }
+
+//     return null;
+// }
